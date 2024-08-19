ac7f66df0afb57ea04862f2ac0061edfbb396fd4
Ltien123 <145563112+LTien123@users.noreply.github.com>
2024-08-19 22:53:34 +0700

    lai

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/components/homepage/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'products',
+  //   loadChildren: () =>
+  //     import('./products/products.module').then((m) => m.ProductsModule),
+  // },
+  {
+    path: '',
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
